ce90a5874a072996781886b0139824c8a7b25254
mamta786 <mamta.barnwal786@gmail.com>
2025-02-25 16:39:22 +0530

    Create 2788.java

---
+++ 2788.java
@@ -0,0 +1,22 @@
+2788. Split Strings by Separator
+
+
+class Solution {
+   public List<String> splitWordsBySeparator(List<String> words, char separator) {
+      
+       ArrayList<String> arlist = new ArrayList<>();
+       ArrayList<String> finallist = new ArrayList<>();
+       for(int i =0;i<words.size();i++){
+         
+          arlist.addAll(Arrays.asList(words.get(i).split(java.util.regex.Pattern.quote(String.valueOf(separator)))));
+          
+       }
+     
+       for(int i =0; i<arlist.size();i++){
+           if(arlist.get(i)!=""){
+            finallist.add(arlist.get(i));
+           }
+       }
+       return finallist;
+   }
+}
